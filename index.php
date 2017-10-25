9401f1c26c27122766e33926d78c8e15271a4541
ThomasJones69 <tjones@ziggo.nl>
2017-10-25 15:05:23 +0200

    Werken met Functions, Optellen en de RETURN statement

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
-    Rekenmachine ("Thomas",2,8);
+
+   optellen(optellen(14,8), optellen(1,1));
+   optellen(22,2);
+   24;
+
+   $var1 = optellen(4,5); //Dus $var1 wordt 9
+   $var1 = 9; //Dus de uitkomst is hetzelfde als de vorige regel
+   $var2 = optellen(2,3); // Dus $var2 wordt dan 5
+   $var3 = optellen(14,7); // Dus $var3 wordt 21
+   echo ($var1 * $var2) / $var3;
+   echo "<br>";
+  
+   echo (optellen(4,5) * optellen(2,3))/optellen(14,7);
+   echo "<br>";
+   echo (9 * 5) / 21; //Zie de code 2 regels hierboven
    
-    function Rekenmachine($Voornaam,$getal1,$getal2){
-        echo "Beste ".$Voornaam;
-        echo "Bij deze schrijf ik u het volgende";
-        echo "<br>";
-    //  echo "Het resulaat van het optellen van ".$getal1." + ".$getal2." is ".$getal1 + $getal2;
-        echo $getal1 + $getal2;
-        echo "<br>";
-    }
-
-   Rekenmachine("Roan",6,8);
-   Rekenmachine("Wes",25,25);
-
+   function optellen($getal1, $getal2){
+   //    echo "De uitkomst is: ".($getal1+$getal2);
+       return ($getal1+$getal2);
+   }
+   
 ?>
